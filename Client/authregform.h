f9278e9a017bb9e7b6125101068148569c974055
Lucrumporcus <146133870+Lucrumporcus@users.noreply.github.com>
2024-06-08 14:01:55 +0300

    комменты authregform.h

--- Client/authregform.h
+++ Client/authregform.h
@@ -1,3 +1,8 @@
+/**
+ * @file AuthRegForm.h
+ * @brief Заголовочный файл для класса AuthRegForm.
+ */
+
 #ifndef AUTHREGFORM_H
 #define AUTHREGFORM_H
 
@@ -5,6 +10,10 @@
 //#include "functionsforclient.h"
 
 namespace Ui {
+/**
+ * @class AuthRegForm
+ * @brief Класс для работы с формой авторизации и регистрации.
+ */
 class AuthRegForm;
 }
 
@@ -13,20 +22,50 @@ class AuthRegForm : public QWidget
     Q_OBJECT
 
 public:
+    /**
+     * @brief Конструктор класса AuthRegForm.
+     * @param parent Родительский виджет, по умолчанию nullptr.
+     */
     explicit AuthRegForm(QWidget *parent = nullptr);
+    /**
+     * @brief Деструктор класса AuthRegForm.
+     */
     ~AuthRegForm();
    // FunctionsForClient* get_func();
 
 private slots:
+    /**
+     * @brief Слот для обработки нажатия кнопки авторизации.
+     */
     void on_pushButtonAuth_clicked();
+
+    /**
+     * @brief Слот для обработки нажатия кнопки изменения данных.
+     */
     void on_pushButtonChange_clicked();
+
+    /**
+     * @brief Слот для обработки нажатия кнопки регистрации.
+     */
     void on_pushButtonReg_clicked();
 
 private:
-    Ui::AuthRegForm *ui;
+    Ui::AuthRegForm *ui; ///< Указатель на интерфейсный объект.
 //    FunctionsForClient func;
 signals:
+    /**
+     * @brief Сигнал для авторизации пользователя.
+     * @param log Логин пользователя.
+     * @param pass Пароль пользователя.
+     */
     void auth(QString log, QString pass);
+
+    /**
+     * @brief Сигнал для регистрации пользователя.
+     * @param log Логин пользователя.
+     * @param pass Пароль пользователя.
+     * @param email Электронная почта пользователя.
+     */
     void reg(QString log, QString pass, QString email);
 };
 
