e17ffb78c0ecb7a94d95ef8eda649f1db4393727
Lucrumporcus <146133870+Lucrumporcus@users.noreply.github.com>
2024-06-13 02:13:10 +0300

    комменты

--- TCPserver/functionsforserver.h
+++ TCPserver/functionsforserver.h
@@ -5,23 +5,77 @@
 #include <QStringList>
 #include <QByteArray>
 
-// Функция для парсинга данных от клиента
+/**
+ * @brief Парсинг данных от клиента
+ * 
+ * @param data_from_client Данные, полученные от клиента
+ * @return QByteArray Результат парсинга
+ */
 QByteArray parsing(QString data_from_client);
 
-// Функция для обработки авторизации
+/**
+ * @brief Обработка авторизации
+ * 
+ * @param log Логин пользователя
+ * @param pass Пароль пользователя
+ * @return QByteArray Результат авторизации
+ */
 QByteArray auth(QString log, QString pass);
 
-// Функция для обработки регистрации
+/**
+ * @brief Обработка регистрации
+ * 
+ * @param log Логин пользователя
+ * @param pass Пароль пользователя
+ * @param mail Электронная почта пользователя
+ * @return QByteArray Результат регистрации
+ */
 QByteArray reg(QString log, QString pass, QString mail);
 
+/**
+ * @brief Выполнение задачи 1
+ * 
+ * @param text Входные данные для задачи
+ * @return QByteArray Результат выполнения задачи
+ */
 QByteArray task1(const QString &text);
 
+/**
+ * @brief Выполнение задачи 2
+ * 
+ * @param text Входные данные для задачи
+ * @return QByteArray Результат выполнения задачи
+ */
 QByteArray task2(QString text);
 
+/**
+ * @brief Выполнение задачи 3
+ * 
+ * @param func Функция для выполнения
+ * @param x0 Начальное значение
+ * @param x1 Конечное значение
+ * @param elipson Точность
+ * @param maxIter Максимальное количество итераций
+ * @return QByteArray Результат выполнения задачи
+ */
 QByteArray task3(QString func, QString x0, QString x1, QString elipson, QString maxIter);
 
+/**
+ * @brief Получение статистики пользователя
+ * 
+ * @param log Логин пользователя
+ * @return QByteArray Статистика пользователя
+ */
 QByteArray stat(QString log);
 
+/**
+ * @brief Проверка данных пользователя
+ * 
+ * @param log Логин пользователя
+ * @param pass Пароль пользователя
+ * @param mail Электронная почта пользователя
+ * @return QByteArray Результат проверки
+ */
 QByteArray check(QString log, QString pass, QString mail);
 
 #endif // FUNCTIONSFORSERVER_H
