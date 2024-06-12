9bab98987595c3625278d976158b0139660a7320
Lucrumporcus <146133870+Lucrumporcus@users.noreply.github.com>
2024-06-13 02:07:05 +0300

    комменты

--- TCPserver/tcpserver.h
+++ TCPserver/tcpserver.h
@@ -1,3 +1,8 @@
+/**
+ * @file TcpServer.h
+ * @brief Заголовочный файл класса TcpServer.
+ */
+
 #ifndef TCPSERVER_H
 #define TCPSERVER_H
 
@@ -6,21 +11,54 @@
 #include <QTcpSocket>
 #include <QList>
 
+/**
+ * @class TcpServer
+ * @brief Класс для управления TCP сервером.
+ * 
+ * Класс TcpServer отвечает за создание и управление TCP сервером, 
+ * а также за обработку подключений клиентов и передачу данных.
+ */
 class TcpServer : public QObject
 {
     Q_OBJECT
 public:
+    /**
+     * @brief Конструктор класса TcpServer.
+     * @param parent Указатель на родительский объект, по умолчанию nullptr.
+     */
     explicit TcpServer(QObject *parent = nullptr);
+
+    /**
+     * @brief Деструктор класса TcpServer.
+     */
     ~TcpServer();
 
 private slots:
+    /**
+     * @brief Слот, вызываемый при новом подключении клиента.
+     * 
+     * Этот слот обрабатывает новые подключения клиентов к серверу.
+     */
     void slotNewConnection();
+
+    /**
+     * @brief Слот для чтения данных от клиента.
+     * 
+     * Этот слот вызывается при получении данных от подключенного клиента.
+     */
     void slotServerRead();
+
+    /**
+     * @brief Слот, вызываемый при отключении клиента.
+     * 
+     * Этот слот обрабатывает отключение клиента от сервера.
+     */
     void slotClientDisconnected();
 
 private:
-    QTcpServer *mTcpServer;
-    QList<QTcpSocket*> mTcpSockets;
+    QTcpServer *mTcpServer; /**< Указатель на объект QTcpServer для управления сервером. */
+    QList<QTcpSocket*> mTcpSockets; /**< Список сокетов для управления подключенными клиентами. */
 };
 
-#endif
+#endif // TCPSERVER_H
+
