608f20355a74634cca18a661602192a48748da5e
Lucrumporcus <146133870+Lucrumporcus@users.noreply.github.com>
2024-06-13 02:15:31 +0300

    комменты

--- TCPserver/database.h
+++ TCPserver/database.h
@@ -11,34 +11,94 @@
 #include <QVariant>
 #include <QStringList>
 
+/**
+ * @class DataBase
+ * @brief Класс для работы с базой данных, реализующий паттерн одиночка.
+ */
 class DataBase;
 
+/**
+ * @class DatabaseDestroyer
+ * @brief Класс для правильного уничтожения объекта DataBase.
+ */
 class DatabaseDestroyer
 {
 private:
-    DataBase * p_instance;
+    DataBase * p_instance; ///< Указатель на экземпляр DataBase.
 public:
+    /**
+     * @brief Деструктор класса DatabaseDestroyer.
+     */
     ~DatabaseDestroyer();
+
+    /**
+     * @brief Инициализирует указатель на экземпляр DataBase.
+     * @param p Указатель на экземпляр DataBase.
+     */
     void initialize(DataBase * p);
 };
 
+/**
+ * @class DataBase
+ * @brief Класс для работы с базой данных, реализующий паттерн одиночка.
+ */
 class DataBase {
 private:
-    static DataBase *p_instance;
-    static DatabaseDestroyer destroyer;
-    QSqlDatabase db;
+    static DataBase *p_instance; ///< Указатель на единственный экземпляр класса.
+    static DatabaseDestroyer destroyer; ///< Экземпляр класса DatabaseDestroyer для уничтожения объекта.
+    QSqlDatabase db; ///< Объект базы данных.
+
+    /**
+     * @brief Конструктор класса DataBase.
+     */
     DataBase();
+
+    /**
+     * @brief Деструктор класса DataBase.
+     */
     ~DataBase();
+
+    /**
+     * @brief Конструктор копирования удален.
+     */
     DataBase(const DataBase&) = delete;
+
+    /**
+     * @brief Оператор присваивания удален.
+     */
     void operator= (const DataBase&) = delete;
+
+    /**
+     * @brief Класс DatabaseDestroyer является другом для DataBase.
+     */
     friend class DatabaseDestroyer;
 
 public:
+    /**
+     * @brief Создает таблицу в базе данных.
+     * @return Возвращает true в случае успешного создания таблицы, иначе false.
+     */
     bool createTable();
+
+    /**
+     * @brief Возвращает единственный экземпляр класса DataBase.
+     * @return Ссылка на экземпляр DataBase.
+     */
     static DataBase& getInstance();
+
+    /**
+     * @brief Возвращает объект базы данных.
+     * @return Ссылка на объект QSqlDatabase.
+     */
     QSqlDatabase& getDatabase();
-    QStringList queryToDatabase(QStringList);
+
+    /**
+     * @brief Выполняет запрос к базе данных.
+     * @param query Список строк с SQL-запросами.
+     * @return Список строк с результатами выполнения запросов.
+     */
+    QStringList queryToDatabase(QStringList query);
 };
 
-
 #endif
+
